9ef1f0f1459e2552ad2aa41cead32e08b96ebed9
LehaBruev <115392339+LehaBruev@users.noreply.github.com>
2025-04-01 21:17:39 +0800

    //пп

--- Git_Kraken_Training/Program.cs
+++ Git_Kraken_Training/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace Git_Kraken_Training
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("vetka2 vtoraya popitka");
+            Console.WriteLine("Кракен ветка 1 на ветке 2 = ответвление");
             Console.WriteLine(Console.ReadLine());
 
 
